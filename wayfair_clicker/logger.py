5a39c26aabbf4c01d6a34847926c6a63274bd374
v.kondratov <ubwork@UBwork.myguest.virtualbox.org>
2025-10-20 22:05:25 +0500

    logger

--- wayfair_clicker/logger.py
+++ wayfair_clicker/logger.py
@@ -7,8 +7,8 @@ def setup_logger():
     """Настройка логгера для бота"""
 
     # Создаем папку для логов если нет
-    if not os.path.exists('logs'):
-        os.makedirs('logs')
+    # if not os.path.exists('logs'):
+    #     os.makedirs('logs')
 
     # Форматирование
     formatter = logging.Formatter(
@@ -21,18 +21,18 @@ def setup_logger():
     logger.setLevel(logging.INFO)
 
     # Файловый обработчик
-    file_handler = logging.FileHandler(
-        f'logs/bot_{datetime.now().strftime("%Y%m%d")}.log',
-        encoding='utf-8'
-    )
-    file_handler.setFormatter(formatter)
+    # file_handler = logging.FileHandler(
+    #     f'logs/bot_{datetime.now().strftime("%Y%m%d")}.log',
+    #     encoding='utf-8'
+    # )
+    # file_handler.setFormatter(formatter)
 
     # Консольный обработчик
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
 
     # Добавляем обработчики
-    logger.addHandler(file_handler)
+    # logger.addHandler(file_handler)
     logger.addHandler(console_handler)
 
     return logger
